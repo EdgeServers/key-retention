00ed448df4045ccaaba8dbdf1bd45d604d40ea4f
kris <kris@edgeservers.com.au>
2024-08-01 06:32:18 +0000

    WIP

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="B34khZHZLvgjZeOlaX9vFjHqKIBjrV7l4JG8jzqZ46W3WXHNBxjTatrZCwGQFt3N",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "54.79.242.17"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "key-retention.hourly.one"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
